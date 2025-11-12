cade57e1f64eec3e33304b72f594094fad7aa706
R-Grigala <r.grigalashvili777@gmail.com>
2025-11-12 16:02:38 +0400

    add scheduler

--- tools/scheduler.py
+++ tools/scheduler.py
@@ -20,3 +20,18 @@ MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD_STR')
 # გარემოს ცვლადების შემოწმება
 if not all([MYSQL_HOST, MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD]):
     raise ValueError("მონაცემთა ბაზის პარამეტრები დაკარგულია!")
+
+def connection_db():
+    # მონაცემთა ბაზასთან კავშირის ტესტი
+    try:
+        connection = pymysql.connect(
+            host=MYSQL_HOST,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            database=MYSQL_DATABASE
+        )
+        logging.info("მონაცემთა ბაზასთან კავშირი წარმატებით შესრულდა.")
+        return connection
+    except pymysql.MySQLError as e:
+        logging.error(f"მონაცემთა ბაზასთან კავშირი ვერ მოხერხდა: {e}")
+        raise  # შეცდომის გადაცემა, რომ პროგრამა შეწყდეს
